d90116084cd525385169425f1d90065a129fdbad
ChanceBarimbao <chance.barimbao@oit.edu>
2025-08-11 11:31:00 -0700

    removed unused items for tutorial

--- backend/mod.ts
+++
@@ -1,3 +0,0 @@
-export { default as PollrLookupServiceFactory } from './src/lookup-services/PollrLookupServiceFactory.js'
-export { default as PollrTopicManager } from './src/topic-managers/PollrTopicManager.js'
-export * from './src/types.js'
